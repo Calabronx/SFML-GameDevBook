3adc58581d6ad72ca04ab7f6b46515fb81f165fd
Calabronx <sebicala@hotmail.com>
2023-12-05 20:27:25 -0300

    start of chapter 6

---
+++ source/view/component.cpp
@@ -0,0 +1,35 @@
+#include "component.hpp"
+
+GUI::Component::Component()
+{
+}
+
+GUI::Component::~Component()
+{
+}
+
+bool GUI::Component::isSelected() const
+{
+    return false;
+}
+
+void GUI::Component::select()
+{
+}
+
+void GUI::Component::deselect()
+{
+}
+
+bool GUI::Component::isActive() const
+{
+    return false;
+}
+
+void GUI::Component::activate()
+{
+}
+
+void GUI::Component::deactivate()
+{
+}
